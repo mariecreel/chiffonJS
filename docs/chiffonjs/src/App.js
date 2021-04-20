51818df5c968b1e16e4ebd436cac779562bc4468
ncreel <nick.creel@protonmail.com>
2021-04-19 20:50:49 -0400

    run npx create react app, remove boilerplate photos, add logo header

---
+++ docs/chiffonjs/src/App.js
@@ -0,0 +1,16 @@
+import logo from './chiffonJSlogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo-wrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
