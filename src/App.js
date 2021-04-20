a95af2bac4a6aa0158528c29d9c764bb3fea199a
ncreel <nick.creel@protonmail.com>
2021-04-20 16:12:49 -0400

    add .env SKIP_PREFLIGHT_CHECK, add favicons, reinstall dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logo from './images/chiffonJSlogo.png';
 
 function App() {
   return (
     <div className="App">
-      <p>hello, world</p>
+      <main>
+      blah blah need stuff here
+      </main>
     </div>
   );
 }
